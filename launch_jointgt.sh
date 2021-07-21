0d05b75477295c83e3e9472bd1ac00479fd30b4b
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2021-07-21 14:07:21 +0800

    Update launch_jointgt.sh

--- launch_jointgt.sh
+++ launch_jointgt.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH -c 2
+#SBATCH --job-name=jointGT  
+#SBATCH --partition=longq
+#SBATCH --account=pawsey0001
+#SBATCH --mem=8000
+#SBATCH --time=96:00:00
+#SBATCH --export=NONE
 
+module load java
 conda activate gatk4_pipeline
 
-java -Dconfig.file=/data/cromwellGATK4/local.conf -jar /data/cromwellGATK4/cromwell-61.jar run -i Multisample_jointgt_GATK4_inputs_hg38.json
+java -Dconfig.file=/scratch/pawsey0001/sbeecroft/cromwell2/slurm.conf -jar /scratch/pawsey0001/sbeecroft/tool/cromwell-58.jar run /scratch/pawsey0001/sbeecroft/cromwell2/Multisample_jointgt_GATK4.wdl \
+   -i /scratch/pawsey0001/sbeecroft/cromwell2/Multisample_jointgt_GATK4_inputs_hg38.json \
+   -o /scratch/pawsey0001/sbeecroft/cromwell2/cromwell.options
